ebac76ffead9c0a8d275545d5ecf80717e53ab40
Evgeny <evgenygorokhovg@yahoo.com>
2024-12-25 20:29:36 +0300

    Update commit

--- main.py
+++ main.py
@@ -1,22 +1,71 @@
 import os
+from urllib.parse import urlparse
 
 from dotenv import load_dotenv
 import requests
 
 
-def main():
-    load_dotenv()
-    service_token = os.getenv('TOKEN')
-    url = 'https://rutube.ru/feeds/popular/'
-    method = 'method/utils.getShortLink'
+def shorten_link(url, token):
+    method = 'https://api.vk.com/method/utils.getShortLink'
     payload = {
         'url': url,
-        "access_token": service_token,
+        "access_token": token,
         'v': 5.199
     }
-    response = requests.get(url + method, params=payload)
+    response = requests.get(method, params=payload)
     response.raise_for_status()
-    print(response.json())
+    method_info = response.json()
+    short_url = method_info['response']['short_url']
+    return short_url
+
+
+def count_clicks(url, token):
+    parsed = urlparse(url)
+    url = parsed.path.replace('/', '')
+    method = 'https://api.vk.com/method/utils.getLinkStats'
+    payload = {
+        'key': url,
+        "access_token": token,
+        'v': 5.199
+    }
+    response = requests.get(method, params=payload)
+    response.raise_for_status()
+    method_info = response.json()
+    number_of_clicks = method_info['response']['stats'][0]['views']
+    return number_of_clicks
+
+
+def is_shorten_link(url, token):
+    method = 'https://api.vk.com/method/utils.checkLink'
+    payload = {
+        'url': url,
+        "access_token": token,
+        'v': 5.199
+    }
+    response = requests.get(method, params=payload)
+    response.raise_for_status()
+    method_info = response.json()
+    checked_link = method_info['response']['link']
+
+    if checked_link != url:
+        return True
+
+    return False
+
+
+def main():
+    load_dotenv()
+    token = os.getenv('TOKEN')
+    url = input('Введите ссылку для сокращения: ')
+
+    try:
+        if is_shorten_link(url, token):
+            print(count_clicks(url, token))
+        else:
+            print(shorten_link(url, token))
+    except KeyError:
+        print('Ошибка, неправильно указана ссылка.')
+
 
 if __name__ == '__main__':
     main()
